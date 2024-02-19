d14d3140aee616e1d09baec925a3446c7a5dfb97
Sparkkk <89287792+Hsusparkkk@users.noreply.github.com>
2024-02-20 01:07:10 +0800

    Add files via upload

--- music.js
+++ music.js
@@ -2,6 +2,8 @@ const music = new Audio("chipichapa.mp3")
 
 music.volume = 0.7
 music.loop = true
-music.play()
-
 const mp3Input = document.getElementById("mp3Input")
+
+body.addEventListener("mousemove",()=>{
+    music.play()
+})
